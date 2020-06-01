84f276ba0d575595c3d7dabc51a0fd8e8d61c3b6
Lavish <kumar.lavish.0109@gmail.com>
2020-06-01 19:13:52 +0530

    #

--- Question_Mark/routes.py
+++ Question_Mark/routes.py
@@ -105,7 +105,7 @@ def delete_ques(id):
     return redirect(url_for('profile'))
 
 
-@app.route("/user_posts/<string:username>")
+@app.route("/user_posts/<string:username>", methods=['POST'])
 def user_posts(username):
     user = User.query.filter_by(username=username).first_or_404()
     quest = Questions.query.filter_by(author=user)\
